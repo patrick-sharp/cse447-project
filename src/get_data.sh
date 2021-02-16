bf3bd5f3e9d7bd848d86fa692057e36ce2f950b1
Zack Barnes <zbarnes@cs.washington.edu>
2021-02-16 13:24:46 -0800

    add to topics list for download and itemize list of downloaded articles

--- src/get_data.sh
+++ src/get_data.sh
@@ -4,4 +4,31 @@ python3 load_wiki.py
 [ -d data ] || mkdir data
 mv *.txt data
 totalFiles=$(ls data | wc -l)
-echo "Number of articles downloaded was $totalFiles"
+numMandarin=$(ls data/*zh.txt | wc -l)
+numCantonese=$(ls data/*zh-yue.txt | wc -l)
+numEnglish=$(ls data/*en.txt | wc -l)
+numItalian=$(ls data/*it.txt | wc -l)
+numGerman=$(ls data/*de.txt | wc -l)
+numJaponese=$(ls data/*ja.txt | wc -l)
+numRussian=$(ls data/*ru.txt | wc -l)
+numSpanish=$(ls data/*es.txt | wc -l)
+numFrench=$(ls data/*fr.txt | wc -l)
+numNorwegian=$(ls data/*no.txt | wc -l)
+numDutch=$(ls data/*nl.txt | wc -l)
+numDanish=$(ls data/*da.txt | wc -l)
+numSwedish=$(ls data/*sv.txt | wc -l)
+echo "-------------- Finished Downloading --------------"
+echo "Total number of articles downloaded was $totalFiles"
+echo "Number of articles downloaded in Mandarin: $numMandarin"
+echo "Number of articles downloaded in Cantonese: $numCantonese"
+echo "Number of articles downloaded in English: $numEnglish"
+echo "Number of articles downloaded in Italian: $numItalian"
+echo "Number of articles downloaded in German: $numGerman"
+echo "Number of articles downloaded in Japonese: $numJaponese"
+echo "Number of articles downloaded in Russian: $numRussian"
+echo "Number of articles downloaded in Spanish: $numSpanish"
+echo "Number of articles downloaded in French: $numFrench"
+echo "Number of articles downloaded in Norwegian: $numNorwegian"
+echo "Number of articles downloaded in Dutch: $numDutch"
+echo "Number of articles downloaded in Danish: $numDanish"
+echo "Number of articles downloaded in Swedish: $numSwedish"
--- src/load_wiki.py
+++ src/load_wiki.py
@@ -1,5 +1,7 @@
 import wikipedia
 import sys
+from google_trans_new import google_translator
+
 """
     Need articles in:
         * Mandarin - zh
@@ -23,30 +25,40 @@ import sys
 """
 languages = ['zh', 'zh-yue', 'en', 'it', 'de', 'ja', \
             'ru', 'es', 'fr', 'no', 'nl', 'da', 'sv']
-wip2language = {'zh': 'Mandarin', 'zh-yue': 'Cantonese', 'en': 'English', 'it' : 'Italian', 'de': 'German', 'ja': 'Japanese', \
-            'ru': 'Russian', 'es': 'Spanish', 'fr': 'French', 'no': 'Norwegan', 'nl' : 'Dutch', 'da': 'Danish', 'sv': 'Sweedish'}
-topics = ['outer space', 'Interstellar travel', 'Interplanetary spaceflight', 'International Space Station']
-print("Saving Articles...")
-# saving files as article_lang.txt
-for lang in languages:
+trans_lang = ['zh-tw', 'zh-cn', 'en', 'it', 'de', 'ja', \
+            'ru', 'es', 'fr', 'no', 'nl', 'da', 'sv']
+wip2language = {'zh': 'chinese (traditional)', 'zh-yue': 'chinese (simplified)', 'en': 'English', 'it' : 'Italian', 'de': 'German', 'ja': 'Japanese', \
+            'ru': 'Russian', 'es': 'Spanish', 'fr': 'French', 'no': 'Norwegan', 'nl' : 'Dutch', 'da': 'Danish', 'sv': 'Swedish'}
+topics = ['Mars', 'Moon', 'Moon landing', 'astronaut', 'outer space', 'Interstellar travel', 'Interplanetary spaceflight', 'International Space Station', \
+        'galaxy', 'solar system', 'spaceship', 'Milky way galaxy', 'planetary orbit']
+
+translator = google_translator()
+
+print("Begin Downloading Articles...")
+# saving article_lang.txt
+for i, lang in enumerate(languages):
     for topic in topics:
         wikipedia.set_lang(lang)
-        # take spaces out of topics
-        txt_filename = ""
+        string = ""
         topic_tokens = topic.split()
         for i, token in enumerate(topic_tokens):
             if i < (len(topic_tokens) - 1):
-                txt_filename += token + "-"
+                string += token + "-"
             else:
-                txt_filename += token
+                string += token
 
         try:
-            page_content = wikipedia.page(topic).content
-            with open(txt_filename + "_" + lang + ".txt", 'w') as f:
-                print(wikipedia.page(topic).content, file = f)
+            #import pdb; pdb.set_trace()
+            translated_topic = translator.translate(topic, lang_tgt=trans_lang[i])
+            #page = wikipedia.page(topic).content
+            page = wikipedia.page(translated_topic).content
+            with open(string + "_" + lang + ".txt", 'w') as f:
+                print(wikipedia.page(translated_topic).content, file = f)
+            print(f'Successfully downloaded an article on {topic} in {wip2language[lang]}')
         except wikipedia.DisambiguationError:
             print(f'Disambiguation error on {topic} in {wip2language[lang]}')
             continue
         except wikipedia.PageError:
             print(f'Page error on {topic} in {wip2language[lang]}')
             continue
+                
